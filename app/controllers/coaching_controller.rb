3c24720b2eede43ca365c0bb8e7d677135f94655
Sarah Willis <sarahbethwillis@gmail.com>
2017-04-17 14:20:21 +0200

    coach question

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,20 @@
+class CoachingController < ApplicationController
+
+
+
+  def answer
+    @question = params[:query]
+      if @question.include? "?"
+        @answer = "Silly question, get dressed and go to work!"
+      elsif @question == "I am going to work right now!"
+        @answer = ""
+      else
+        @answer = "I don't care, get dressed and go to work!"
+      end
+  end
+
+  def ask
+  end
+end
+
+
